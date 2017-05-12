1fa27000138ed7e8d471687d15429a8fb21f9e45
Kevin J Baird <tempestc@hotmail.com>
2017-05-12 18:08:12 -0400

    First commit.

--- StorjshareDynamite/Default.aspx
+++ StorjshareDynamite/Default.aspx
@@ -1,42 +1,50 @@
 ﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="StorjshareDynamite._Default" %>
 
+<%@ Register Assembly="Telerik.Web.UI" Namespace="Telerik.Web.UI" TagPrefix="telerik" %>
+<%@ Register Assembly="Telerik.Web.UI.Skins" Namespace="Telerik.Web.UI" TagPrefix="telerik" %>
+
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
 
-    <div class="jumbotron">
-        <h1>ASP.NET</h1>
-        <p class="lead">ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS, and JavaScript.</p>
-        <p><a href="http://www.asp.net" class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
+
+
+    <telerik:RadAjaxManager ID="RadAjaxManager1" runat="server">
+    </telerik:RadAjaxManager>
+    <telerik:RadScriptManager ID="RadScriptManager1" runat="server">
+        <Scripts>
+            <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.Core.js"></asp:ScriptReference>
+            <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.jQuery.js"></asp:ScriptReference>
+            <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.jQueryInclude.js"></asp:ScriptReference>
+        </Scripts>
+    </telerik:RadScriptManager>
+    <telerik:RadStyleSheetManager ID="RadStyleSheetManager1" runat="server">
+    </telerik:RadStyleSheetManager>
+    <div class="demo-container size-narrow">
+        <div class="qsf-demo-canvas qsf-demo-canvas-overview">
+            <p>Upload your log</p>
+            <telerik:RadCloudUpload ID="RadCloudUpload1" runat="server" RenderMode="Lightweight" OnClientUploadFailed="onClientUploadFailed" OnFileUploaded="RadCloudUpload1_FileUploaded" ProviderType="Azure" MaxFileSize="20971520">
+            </telerik:RadCloudUpload>
+
+        </div>
+        <div><br /><p>Once your upload completes.  Push Process to start checking your log.</p><asp:Button ID="btnProcess" runat="server" Text="Process" /></div>
+        <div><br /><p>Node ID: <asp:Label ID="lblNodeID" runat="server" Text="..."></asp:Label></p></div>
+        <div><br /><p>Delta: <asp:Label ID="lblDelta" runat="server" Text="..."></asp:Label></p></div>
+        <div><br /><p>Status: <asp:Label ID="lblStatus" runat="server" Text="..."></asp:Label></p></div>
+        <div><p>Receiving: <asp:Label ID="lblReceiving" runat="server" Text="..."></asp:Label></p></div>
+        <div><asp:Label ID="lblOFFERS" runat="server" Text=""></asp:Label></div>
+
+
     </div>
 
-    <div class="row">
-        <div class="col-md-4">
-            <h2>Getting started</h2>
-            <p>
-                ASP.NET Web Forms lets you build dynamic websites using a familiar drag-and-drop, event-driven model.
-            A design surface and hundreds of controls and components let you rapidly build sophisticated, powerful UI-driven sites with data access.
-            </p>
-            <p>
-                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301948">Learn more &raquo;</a>
-            </p>
-        </div>
-        <div class="col-md-4">
-            <h2>Get more libraries</h2>
-            <p>
-                NuGet is a free Visual Studio extension that makes it easy to add, remove, and update libraries and tools in Visual Studio projects.
-            </p>
-            <p>
-                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301949">Learn more &raquo;</a>
-            </p>
-        </div>
-        <div class="col-md-4">
-            <h2>Web Hosting</h2>
-            <p>
-                You can easily find a web hosting company that offers the right mix of features and price for your applications.
-            </p>
-            <p>
-                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301950">Learn more &raquo;</a>
-            </p>
-        </div>
-    </div>
+    <telerik:RadScriptBlock runat="server">
+        <script type="text/javascript">
+
+            function onClientUploadFailed(sender, eventArgs) {
+                alert(eventArgs.get_message())
+            }
+
+        </script>
+    </telerik:RadScriptBlock>
+
+
 
 </asp:Content>
--- StorjshareDynamite/Default.aspx.cs
+++ StorjshareDynamite/Default.aspx.cs
@@ -4,6 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Telerik.Web.UI.Skins;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Web.Configuration;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace StorjshareDynamite
 {
@@ -12,6 +22,124 @@ namespace StorjshareDynamite
         protected void Page_Load(object sender, EventArgs e)
         {
 
+
         }
+
+        protected void RadCloudUpload1_FileUploaded(object sender, Telerik.Web.UI.CloudFileUploadedEventArgs args)
+        {
+
+            string strOriginalFileName = args.FileInfo.OriginalFileName;
+            string strFileName = args.FileInfo.KeyName;
+            try
+            {
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("Azure"));
+                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+                CloudBlobContainer blobContainer = blobClient.GetContainerReference("storjsharedynamite");
+                CloudBlockBlob fileBlob = blobContainer.GetBlockBlobReference(strFileName);
+
+                using (StreamReader reader = new StreamReader(fileBlob.OpenRead()))
+                {
+
+                    string strLog = reader.ReadToEnd();
+                    if (strLog.LastIndexOf("with nodeID") != -1)
+                    {
+                        string strNodeIDtmp = strLog.Substring(strLog.LastIndexOf("with nodeID") + 12);
+                        string strNodeID = strNodeIDtmp.Substring(0, strNodeIDtmp.IndexOf("\""));
+                        lblNodeID.Text = strNodeID;
+
+
+                    }
+
+
+
+                    if (strLog.LastIndexOf("delta") != -1)
+                    {
+                        string strDelta = strLog.Substring(strLog.LastIndexOf("delta") + 7);
+                        string strDeltaResult = strDelta.Substring(0, strDelta.IndexOf("ms"));
+                        int intDelta = Convert.ToInt32(strDeltaResult);
+                        if (intDelta > 500 || intDelta < -500)
+                        {
+
+                            lblDelta.ForeColor = System.Drawing.Color.Red;
+                            lblDelta.Text = strDeltaResult + " |  Your clock is out of sync.  If on Windows please download the Time Sync Tool at www.timesynctool.com";
+
+                        }
+                        else
+                        {
+                            lblDelta.ForeColor = System.Drawing.Color.Green;
+                            lblDelta.Text = strDeltaResult + " | Good";
+                        }
+
+                        // int count = new Regex(Regex.Escape("PUBLISH")).Matches(strDeltaResult).Count;
+                        int count = Regex.Matches(Regex.Escape(strDelta), "PUBLISH").Count;
+                        // int offercount = new Regex(Regex.Escape("OFFER")).Matches(strDeltaResult).Count;
+                        int offercount = Regex.Matches(Regex.Escape(strDelta), "OFFER").Count;
+                        // int validcount = new Regex(Regex.Escape("received valid")).Matches(strDeltaResult).Count;
+                        int validcount = Regex.Matches(Regex.Escape(strDelta), "received valid").Count;
+
+                        if (count > 0)
+                        {
+                            lblReceiving.ForeColor = System.Drawing.Color.Green;
+                            lblReceiving.Text = " Yes! You have received " + count.ToString() + " storage messages. | Good.";
+                            if (offercount > 0)
+                            {
+                                lblOFFERS.ForeColor = System.Drawing.Color.Green;
+                                lblOFFERS.Text = "You have sent " + offercount.ToString() + " storage offers.  | Good.";
+                            }
+                            else
+                            {
+                                lblOFFERS.Text = "";
+                            }
+                        }
+                        else
+                        {
+                            lblReceiving.ForeColor = System.Drawing.Color.Goldenrod;
+                            if (validcount > 0)
+                            {
+                                lblReceiving.Text = " Yes.  You are receiving responses. However, no requests for storage (PUBLISH) have been received yet. This is normal if your log is less than 1 hour old.";
+                                lblOFFERS.Text = "";
+                            }
+                            else
+                            { 
+                                lblReceiving.Text = " No.  If other messages don't show an error, your log may be less than an hour old. Please submit again after 1 hour.";
+                                lblOFFERS.Text = "";
+                            }
+                        }
+
+
+
+                    }
+                    if (strLog.LastIndexOf("your address is public and traversal strategies are disabled") != -1)
+                    {
+                        lblStatus.ForeColor = System.Drawing.Color.Green;
+                        lblStatus.Text = " | Your address is public and traversal strategies are disabled.  Good.";
+                    }
+                    else
+                    {
+                        lblStatus.Text = "";
+                    }
+
+
+
+
+
+
+
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception: " + ex.Message);
+                return;
+            }
+
+
+
+
+        }
+
     }
 }
